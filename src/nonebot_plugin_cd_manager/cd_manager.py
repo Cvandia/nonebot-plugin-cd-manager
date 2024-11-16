28527d1f16229695b3dbc1c9aacb7954d701e6e9
Cvandia <106718176+Cvandia@users.noreply.github.com>
2024-11-16 12:13:57 +0800

    ⚡️ 优化cd管理逻辑，简化代码并减少冗余检查

--- src/nonebot_plugin_cd_manager/cd_manager.py
+++ src/nonebot_plugin_cd_manager/cd_manager.py
@@ -1,6 +1,7 @@
 """
 Description: 用于管理命令的cd
 """
+
 import time
 from .data_manager import DataManager
 
@@ -16,40 +17,34 @@ def check_if_in_cd(
 
     Return:
         tuple[bool, float]: 是否在cd中, 剩余时间"""
-    in_cd = False
-    remaining_time = 0
-
     # 先检查全局cd
     try:
         data_command = plugin_data.data["all"].get(command)
     except AttributeError:
-        data_command = None
-
+        return False, 0
     if data_command:
         cd = data_command[0]
         last_time = data_command[1]
         # 判断是否在cd中
         if time.time() - last_time < cd:
-            in_cd = True
-            remaining_time = cd - (time.time() - last_time)
+            return True, cd - (time.time() - last_time)
         else:
             data_command[1] = time.time()
+            return False, 0
 
     # 再检查群组cd
-    if not in_cd:
-        try:
-            data_command = plugin_data.data["group"].get(group_id).get(command)
-        except AttributeError:
-            data_command = None
+    try:
+        data_command = plugin_data.data["group"].get(group_id).get(command)
+    except AttributeError:
+        return False, 0
+    if data_command:
+        cd = data_command[0]
+        last_time = data_command[1]
+        # 判断是否在cd中
+        if time.time() - last_time < cd:
+            return True, cd - (time.time() - last_time)
+        else:
+            data_command[1] = time.time()
+            return False, 0
 
-        if data_command:
-            cd = data_command[0]
-            last_time = data_command[1]
-            # 判断是否在cd中
-            if time.time() - last_time < cd:
-                in_cd = True
-                remaining_time = cd - (time.time() - last_time)
-            else:
-                data_command[1] = time.time()
-
-    return in_cd, remaining_time
+    return False, 0
