a2b537e39257b71ec2a706d294641a234333d64c
Cvandia <106718176+Cvandia@users.noreply.github.com>
2024-11-21 17:07:54 +0800

    ✨ feat(cd_manager): 支持三种命令匹配方式
    - 在配置文件中添加 match_rule 字段，可选值为 'full'、'start' 和 'in'
    - 重构 check_if_in_cd 函数，支持三种匹配方式
    - 优化代码结构，提高可读性和可维护性

--- src/nonebot_plugin_cd_manager/cd_manager.py
+++ src/nonebot_plugin_cd_manager/cd_manager.py
@@ -6,10 +6,14 @@ import time
 import random
 from nonebot.matcher import Matcher
 from .data_manager import DataManager
+from .config import config
+
+
+match_rule = config.match_rule
 
 
 def check_if_in_cd(
-    plugin_data: DataManager, group_id: str | int, command: str
+    plugin_data: DataManager, group_id: str | int, event_message: str
 ) -> tuple[bool, float]:
     """检查是否在cd中
     Args:
@@ -19,37 +23,30 @@ def check_if_in_cd(
 
     Return:
         tuple[bool, float]: 是否在cd中, 剩余时间"""
-    # 先检查全局cd
-    try:
-        data_command = plugin_data.data["all"].get(command)
-    except AttributeError:
+
+    def check_cd(data_command):
+        for command, (cd, last_time) in data_command.items():
+            if (
+                (match_rule == "in" and command in event_message)
+                or (match_rule == "full" and command == event_message)
+                or (match_rule == "start" and event_message.startswith(command))
+            ):
+                if time.time() - last_time < cd:
+                    return True, cd - (time.time() - last_time)
+                else:
+                    data_command[command][1] = time.time()
+                    return False, 0
         return False, 0
-    if data_command:
-        cd = data_command[0]
-        last_time = data_command[1]
-        # 判断是否在cd中
-        if time.time() - last_time < cd:
-            return True, cd - (time.time() - last_time)
-        else:
-            data_command[1] = time.time()
-            return False, 0
+
+    # 先检查全局cd
+    global_cd = plugin_data.data.get("all", {})
+    in_cd, remain_time = check_cd(global_cd)
+    if in_cd:
+        return in_cd, remain_time
 
     # 再检查群组cd
-    try:
-        data_command = plugin_data.data["group"].get(group_id).get(command)
-    except AttributeError:
-        return False, 0
-    if data_command:
-        cd = data_command[0]
-        last_time = data_command[1]
-        # 判断是否在cd中
-        if time.time() - last_time < cd:
-            return True, cd - (time.time() - last_time)
-        else:
-            data_command[1] = time.time()
-            return False, 0
-
-    return False, 0
+    group_cd = plugin_data.data.get("group", {}).get(group_id, {})
+    return check_cd(group_cd)
 
 
 async def send_random_cd_message(matcher: Matcher, remain_time: float):
--- src/nonebot_plugin_cd_manager/config.py
+++ src/nonebot_plugin_cd_manager/config.py
@@ -2,6 +2,7 @@
 Description: 配置文件
 """
 
+from typing import Literal
 from pydantic import BaseModel
 from nonebot.plugin import get_plugin_config
 
@@ -12,6 +13,7 @@ class Config(BaseModel):
     """
 
     data_parent_path: str = "./data/cd_manager"
+    match_rule: Literal["full", "start", "in"] = "in"
 
 
 config = get_plugin_config(Config)
